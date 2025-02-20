9dc26e8315c6eb08adfa709eaa93f46a5c439a80
Krzysztof Tomaszewski <drteski@gmail.com>
2025-02-20 16:08:00 +0100

    init

--- app/page.js
+++ app/page.js
@@ -1,101 +1,85 @@
-import Image from "next/image";
+'use client';
+import { Slider } from '@/components/ui/slider';
+import { Label } from '@/components/ui/label';
+import { getAperture, getIso, getLuminance, getNd, getShutter } from '@/lib/utils';
+import { useState } from 'react';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	const luminanceData = getLuminance();
+	const isoData = getIso();
+	const apertureData = getAperture();
+	const shutterData = getShutter();
+	const ndData = getNd();
+	const [lux, setLux] = useState(luminanceData.findIndex(item => item.ev === 0));
+	const [iso, setIso] = useState(isoData.findIndex(item => item.ev === 6));
+	const [aperture, setAperture] = useState(apertureData.findIndex(item => item.ev === 0));
+	const [shutter, setShutter] = useState(shutterData.findIndex(item => item.ev === 0));
+	const [nd, setNd] = useState(ndData.findIndex(item => item.ev === 0));
+	console.log(shutterData[shutter]);
+	return (
+		<div className="w-screen h-screen grid grid-rows-[auto_auto] items-center justify-center">
+			<div>
+				<div className="h-[128px] w-[128px] bg-black text-white scale-[4]">
+					<h3 className="text-xs font-[family-name:var(--font-geist-mono)]">LUX {luminanceData[lux].luminance}</h3>
+					<h3 className="text-xs font-[family-name:var(--font-geist-mono)]">ISO {isoData[iso].iso}</h3>
+					<h3 className="text-xs font-[family-name:var(--font-geist-mono)]">APERTURE {apertureData[aperture].aperture}</h3>
+					<h3 className="text-xs font-[family-name:var(--font-geist-mono)]">SHUTTER {shutterData[shutter].shutter}</h3>
+					<h3 className="text-xs font-[family-name:var(--font-geist-mono)]">ND {ndData[nd].nd}</h3>
+					<h3 className="text-xs font-[family-name:var(--font-geist-mono)]">EV {luminanceData[lux].ev}</h3>
+				</div>
+			</div>
+			<div>
+				<div>
+					<Label>LUX</Label>
+					<Slider
+						defaultValue={[lux]}
+						onValueChange={value => setLux(value)}
+						max={luminanceData.length - 1}
+						step={1}
+						className="w-96"
+					/>
+				</div>
+				<div>
+					<Label>ISO</Label>
+					<Slider
+						defaultValue={[iso]}
+						onValueChange={value => setIso(value)}
+						max={isoData.length - 1}
+						step={1}
+						className="w-96"
+					/>
+				</div>
+				<div>
+					<Label>APERTURE</Label>
+					<Slider
+						defaultValue={[aperture]}
+						onValueChange={value => setAperture(value)}
+						max={apertureData.length - 1}
+						step={1}
+						className="w-96"
+					/>
+				</div>
+				<div>
+					<Label>SHUTTER</Label>
+					<Slider
+						defaultValue={[shutter]}
+						onValueChange={value => setShutter(value)}
+						max={shutterData.length - 1}
+						step={1}
+						className="w-96"
+					/>
+				</div>
+				<div>
+					<Label>ND</Label>
+					<Slider
+						defaultValue={[nd]}
+						onValueChange={value => setNd(value)}
+						max={ndData.length - 1}
+						step={1}
+						className="w-96"
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
